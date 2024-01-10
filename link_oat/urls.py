93733b20ac006b2eacf790a9e474e0b41f88fc4a
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-01-10 09:40:43 -0600

    Add create_link.html template and CreateLink view

--- link_oat/urls.py
+++ link_oat/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Links.as_view() , name='links' ),
+    path("link/create", views.CreateLink.as_view(), name="create_link"),
 ]
--- link_oat/views.py
+++ link_oat/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 from .models import Link
 
@@ -8,3 +8,9 @@ class Links(ListView):
     model = Link
     template_name = 'link_oat/links.html'
     context_object_name = 'links'
+    
+class CreateLink(CreateView):
+    model = Link
+    fields = "__all__"
+    template_name = 'link_oat/create_link.html'
+    success_url = reverse_lazy('links')
