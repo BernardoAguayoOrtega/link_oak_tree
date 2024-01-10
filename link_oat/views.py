2a998f76f30ea56224a5a9df7e87341f1b0cc4dd
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-01-10 09:05:47 -0600

    Add link_oat app with URLs and views

--- link_oat/views.py
+++ link_oat/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from .models import Link
 
 # Create your views here.
+class Links(ListView):
+    model = Link
+    template_name = 'link_oat/links.html'
+    context_object_name = 'links'
